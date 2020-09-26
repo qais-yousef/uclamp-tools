6f5ad05207b9d98ebf6ae1662f8bc4daf740752e
Qais Yousef <qyousef@layalina.io>
2020-09-26 20:43:22 +0100

    Add uclampreset-rt: to reset default 1024 boost
    Only required for kernels that doesn't have sysctl default boost control
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@ set -eux
 
 path="`realpath $(dirname "$0")`"
 
-sudo ln -s "$path/uclampreset" /usr/local/bin/uclampreset
-sudo ln -s "$path/uclampcheck" /usr/local/bin/uclampcheck
+sudo ln -sf "$path/uclampreset" /usr/local/bin/uclampreset
+sudo ln -sf "$path/uclampreset" /usr/local/bin/uclampreset-rt
+sudo ln -sf "$path/uclampcheck" /usr/local/bin/uclampcheck
 
 if [ -d "/etc/pm/sleep.d/" ]; then
-	sudo ln -s "$path/uclamp.pm-suspend" /etc/pm/sleep.d/99_uclamp
+	sudo ln -sf "$path/uclamp.pm-suspend" /etc/pm/sleep.d/99_uclamp
 fi
 
 if [ -d "/lib/systemd/system-sleep/" ]; then
-	sudo ln -s "$path/uclamp.system-sleep" /lib/systemd/system-sleep/uclamp
+	sudo ln -sf "$path/uclamp.system-sleep" /lib/systemd/system-sleep/uclamp
 fi
